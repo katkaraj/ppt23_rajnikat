834925407f4d9c7ff5eb0498d2b696c57847013e
Kateřina Rajnišová <katerinarajnisova@Katerina-MacBook-Pro.local>
2023-02-26 18:10:56 +0100

    cv1.1

--- Program.cs
+++ Program.cs
@@ -1,39 +1,46 @@
-﻿using System;
-
-int cislo, hadanecislo;
-//Random rnd = new Random();
-//cislo = rnd.Next(1, 100);
-//int cislo=Random.Shared.Next();
-int NahodneCislo() => Random.Shared.Next(1, 100); //lambda zápis
-cislo = NahodneCislo();
-
+﻿
+int cislo = NahodneCislo();
 while (true)
 {
-    try
-    {
-        
-        Console.WriteLine("Hádej číslo");
-        hadanecislo = Int32.Parse(Console.ReadLine());
+    Console.WriteLine("Hádej číslo");
+    //hadanecislo = Int32.Parse(Console.ReadLine());
+    string? vstup = Console.ReadLine();
+    if (vstup == null) return;
 
-        if (cislo == hadanecislo)
-        {
-            Console.WriteLine("Uhodl jste!");
-            Console.WriteLine("Chcete hrát znovu? Y/N");
-
-        }
-        if (cislo > hadanecislo)
-        {
-            Console.WriteLine("Číslo je větší");
-        }
-        if (cislo < hadanecislo)
-        {
-            Console.WriteLine("Cislo je menší");
-        }
-    } catch(Exception)
+    if(!int.TryParse(vstup, out int hadanecislo))
     {
         Console.WriteLine("Zadejte číslo");
         continue;
-    }   
+    }
+
+    if (cislo == hadanecislo)
+    {
+        Console.WriteLine("Uhodl jste!");
+        Console.WriteLine($"Chcete hrát znovu? (Y/N)");
+        string? hratZnovu = Console.ReadLine();
+        if (hratZnovu != null && hratZnovu.ToLower() == "y")
+        {
+            Console.Clear();
+            cislo = NahodneCislo();
+        }
+        else
+            break;
+
+    }
+    //if (cislo > hadanecislo)
+    //{
+    //    Console.WriteLine("Číslo je větší");
+    //}
+    //if (cislo < hadanecislo)
+    //{
+    //    Console.WriteLine("Cislo je menší");
+    //}
+    string mensiVetsi = cislo < hadanecislo ? "MENŠÍ" : "VĚTŠÍ";
+    Console.WriteLine($"Tvoje číslo je {mensiVetsi} než myšlené číslo. Zkus to znovu");
 }
-Console.WriteLine("Konec");
-Console.ReadKey();
+
+Console.WriteLine("Díky za hru");
+Console.ReadLine();
+
+int NahodneCislo() => Random.Shared.Next(1, 101);
+
